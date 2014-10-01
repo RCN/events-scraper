c67e8d4f64a8067d31aa2df63dafb249a3018696
Restuta <restuta8@gmail.com>
2014-10-01 12:42:21 -0700

    used request-promise + tried using Mocha programmatically

--- test/scrape-usacycling_test.js
+++ test/scrape-usacycling_test.js
@@ -4,12 +4,19 @@
 var usaCyclingScraper = require('../lib/usacycling-scraper.js');
 
 var request = require('request').defaults({jar: true}),
+    requestp = require('request-promise').defaults({jar: true}),
     cheerio = require('cheerio'),
     colors = require('colors').setTheme({
         debug: 'blue',
         error: 'red',
         info: 'cyan'
     });
+
+describe('test', function() {
+    it('should bla', function() {
+       usaCyclingScraper.scrapeEventsFor2014();
+    })
+})
 
 
 describe('usacycling-scraper node module.', function() {
@@ -25,17 +32,39 @@ describe('When calling to "https://www.usacycling.org/events/state_search.php"',
 
         it('should return exactly 19 events', function(done) {
             var eventsSearchUrl = 'https://www.usacycling.org/events/state_search.php';
-            //calling it for the first time to get needed cookies
-            request(eventsSearchUrl + '?state=CN&race=Road&fyear=2014&rrfilter=rr', function(error, response, body) {
-                //calling with 'ajax=2' parameter to get only html event's data
-                request(eventsSearchUrl + '?state=CN&race=Road&fyear=2014&rrfilter=rr' + '&ajax=2', function(error, response, body) {
-                    $ = cheerio.load(body);
+            requestp(eventsSearchUrl + '?state=CN&race=Road&fyear=2014&rrfilter=rr')
+                .then(function() {
+                    return requestp(eventsSearchUrl + '?state=CN&race=Road&fyear=2014&rrfilter=rr'+ '&ajax=2');
+                })
+                .then(function(response) {
+                    $ = cheerio.load(response);
                     var $events = $('.event');
                     $events.length.should.equal(19);
-                    done();
                 })
-            })
+                .done(function() {
+                    done();
+                });
+
+//            //calling it for the first time to get needed cookies
+//            request(eventsSearchUrl + '?state=CN&race=Road&fyear=2014&rrfilter=rr', function(error, response, body) {
+//                //calling with 'ajax=2' parameter to get only html event's data
+//                request(eventsSearchUrl + '?state=CN&race=Road&fyear=2014&rrfilter=rr' + '&ajax=2', function(error, response, body) {
+//                    $ = cheerio.load(body);
+//                    var $events = $('.event');
+//                    $events.length.should.equal(19);
+//                    done();
+//                })
+//            })
         })
+
+//        describe('for each element', function() {
+//            $('.event a.title.ttip').forEach(function(item) {
+//                it(item.text(), function() {
+//                    assert.ok();
+//                })
+//            })
+//        })
+
 
         it('first event should be "2014 San Bruno Mountain Hill Climb"', function() {
             $('.event a.title.ttip').first().text().should.equal('2014 San Bruno Mountain Hill Climb');

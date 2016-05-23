b568baaf34b8d1d71250ce907a212be8f4178bee
Restuta <restuta8@gmail.com>
2016-05-22 22:31:38 -0700

    ES6 + refactoring

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-var scraper = require('./lib/usacycling-scraper');
+/* eslint-disable*/
+//eslint is disabled to preserve babel config formatting that is easier to copy-paste from .babelrc
+
+require('babel-register')({
+  //see https://babeljs.io/docs/usage/options/#options for more config options
+  babelrc: false,
+  "presets": ["es2015-node5", "stage-2"],
+  "plugins": ["add-module-exports"],
+})
+require('babel-polyfill')
+require('./lib/usac-scraper');

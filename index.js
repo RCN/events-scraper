545e0adddaf60a9928bdaa7d4596f5ed5fdff4ca
Restuta <restuta8@gmail.com>
2017-04-13 11:24:18 -0700

    moved racers scraper to separate sub-folder

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ require('babel-register')({
   //see https://babeljs.io/docs/usage/options/#options for more config options
   babelrc: false,
   "presets": ["es2015-node5", "stage-2"],
-  "plugins": ["add-module-exports"],
+  // "plugins": ["add-module-exports"],
 })
 require('babel-polyfill')
-require('./lib/usac-scraper')
+// require('./lib/usac-scraper')
 // require('./lib/scrapers/example-scraper');
+require('./lib/scrapers/usac-racers');

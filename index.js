9179ffa8810d98d116a6e6a58d651bd7d699cb9c
Restuta <restuta8@gmail.com>
2017-07-24 09:10:51 -0700

    add babel-env

--- index.js
+++ index.js
@@ -4,7 +4,13 @@
 require('babel-register')({
   //see https://babeljs.io/docs/usage/options/#options for more config options
   babelrc: false,
-  "presets": ["es2015-node5", "stage-2"],
+  "presets": [
+    ["env", {
+      "targets": {
+        "node": "current"
+      }
+    }]
+  ],
   // "plugins": ["add-module-exports"],
 })
 require('babel-polyfill')

1e8439802dcecb6a9d0167621fcaff429180b6fa
restuta <restuta8@gmail.com>
2017-08-20 11:19:25 -0700

    scraped all racers from last time (about 30k)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* eslint-disable*/
 //eslint is disabled to preserve babel config formatting that is easier to copy-paste from .babelrc
 
+require('dotenv').config()
+
 require('babel-register')({
   //see https://babeljs.io/docs/usage/options/#options for more config options
   babelrc: false,
